4384b9f3a2f3570b8eb9d4566e74252264072cd6
walseb <greatestmailofalltime@gmail.com>
2018-04-15 18:07:17 +0200

    Kan nu röra sig höger och vänster

--- Juppy/Assets/Scripts/Spelare/Juppy.cs
+++ Juppy/Assets/Scripts/Spelare/Juppy.cs
@@ -6,21 +6,32 @@ public class Juppy : MonoBehaviour {
     Rigidbody2D rigidbody2D;
 
     [SerializeField]
-    int jumpForce = 100;
+    int jumpForce = 400;
+
+    [SerializeField]
+    int horizontalMovementSpeed = 7;
 
     // Use this for initialization
     void Start () {
 	rigidbody2D = this.GetComponent<Rigidbody2D>();
     }
 	
-    // Update is called once per frame
-    //void Update () {
-		
-    //}
+    void Update() {
+        if (Input.GetKey("left")){
+	    //rigidbody2D.AddForce(transform.right * -movementForce);
+	    rigidbody2D.velocity = new Vector2(-horizontalMovementSpeed, rigidbody2D.velocity.y);
+	}
+	if (Input.GetKey("right")){
+	    //    rigidbody2D.AddForce(transform.right * movementForce);
+	    rigidbody2D.velocity = new Vector2(horizontalMovementSpeed, rigidbody2D.velocity.y);
+
+	}
+    }
+
     void OnCollisionEnter2D(Collision2D col) {
 	if(col.gameObject.tag == "Platta")
 	{
-	    rigidbody2D.AddForce(transform.up * JumpForce);
+	    rigidbody2D.AddForce(transform.up * jumpForce);
 	}
     }
 }
