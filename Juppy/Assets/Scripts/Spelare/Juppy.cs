2a6b588516f709cce5b3c2212b3ac1ceae0d33b8
walseb <greatestmailofalltime@gmail.com>
2018-04-15 16:23:34 +0200

    Juppy hoppar nu på platformar

---
+++ Juppy/Assets/Scripts/Spelare/Juppy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Juppy : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    int jumpForce = 100;
+
+    // Use this for initialization
+    void Start () {
+	rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+	
+    // Update is called once per frame
+    //void Update () {
+		
+    //}
+    void OnCollisionEnter2D(Collision2D col) {
+	if(col.gameObject.tag == "Platta")
+	{
+	    rigidbody2D.AddForce(transform.up * JumpForce);
+	}
+    }
+}
